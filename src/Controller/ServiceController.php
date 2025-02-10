288c8c67224b4e3eabf4a6fce0d327facf853d30
elmira <elledevelope@gmail.com>
2025-02-10 14:12:27 +0100

    Created filer by type on `/map` page

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,15 +5,46 @@ namespace App\Controller;
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ServiceController extends AbstractController
 {
-    #[Route('/api/services', name: 'api_services', methods: ['GET'])]
-    public function getServices(ServiceRepository $serviceRepository): JsonResponse
-    {
-        $services = $serviceRepository->findAll();
+    // #[Route('/api/services', name: 'api_services', methods: ['GET'])]
+    // public function getServices(ServiceRepository $serviceRepository): JsonResponse
+    // {
+    //     $services = $serviceRepository->findAll();
 
+    //     $data = array_map(function ($service) {
+    //         return [
+    //             'id' => $service->getId(),
+    //             'name' => $service->getName(),
+    //             'type' => $service->getType(),
+    //             'address' => $service->getAddress(),
+    //             'phone' => $service->getPhone(),
+    //             'website' => $service->getWebsite(),
+    //             'latitude' => $service->getLatitude(),
+    //             'longitude' => $service->getLongitude(),
+    //         ];
+    //     }, $services);
+
+    //     return $this->json($data);
+    // }
+
+    #[Route('/api/services', name: 'api_services', methods: ['GET'])]
+    public function getServices(ServiceRepository $serviceRepository, Request $request): JsonResponse
+    {
+        $type = $request->query->get('type'); 
+
+        // If a type filter is provided, fetch services of that type
+        if ($type) {
+            $services = $serviceRepository->findBy(['type' => $type]);
+        } else {
+            // Fetch all services if no filter is applied
+            $services = $serviceRepository->findAll();
+        }
+
+        // Format the service data for JSON response
         $data = array_map(function ($service) {
             return [
                 'id' => $service->getId(),
