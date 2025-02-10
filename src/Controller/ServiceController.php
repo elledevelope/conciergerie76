741d2182629f63cda09cb07d0f9b8d9d4d3f25bf
elmira <elledevelope@gmail.com>
2025-02-10 14:35:18 +0100

    Created `/museum`, `/park`, `/cafe`

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -6,6 +6,7 @@ use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ServiceController extends AbstractController
@@ -34,7 +35,7 @@ class ServiceController extends AbstractController
     #[Route('/api/services', name: 'api_services', methods: ['GET'])]
     public function getServices(ServiceRepository $serviceRepository, Request $request): JsonResponse
     {
-        $type = $request->query->get('type'); 
+        $type = $request->query->get('type');
 
         // If a type filter is provided, fetch services of that type
         if ($type) {
@@ -59,5 +60,41 @@ class ServiceController extends AbstractController
         }, $services);
 
         return $this->json($data);
+    }
+
+    // Route for Cafe
+    #[Route('/cafe', name: 'app_cafe')]
+    public function showCafes(ServiceRepository $serviceRepository): Response
+    {
+        $cafes = $serviceRepository->findBy(['type' => 'cafe']);
+
+        return $this->render('service/list.html.twig', [
+            'places' => $cafes,
+            'type' => 'Cafés',
+        ]);
+    }
+
+    // Route for Museum
+    #[Route('/museum', name: 'app_museum')]
+    public function showMuseums(ServiceRepository $serviceRepository): Response
+    {
+        $museums = $serviceRepository->findBy(['type' => 'museum']);
+
+        return $this->render('service/list.html.twig', [
+            'places' => $museums,
+            'type' => 'Museums',
+        ]);
+    }
+
+    // Route for Park
+    #[Route('/park', name: 'app_park')]
+    public function showParks(ServiceRepository $serviceRepository): Response
+    {
+        $parks = $serviceRepository->findBy(['type' => 'park']);
+
+        return $this->render('service/list.html.twig', [
+            'places' => $parks,
+            'type' => 'Parks',
+        ]);
     }
 }
