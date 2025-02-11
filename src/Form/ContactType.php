623e2457a15d13431717face5bd5b4879760a03b
elmira <elledevelope@gmail.com>
2025-02-11 21:17:57 +0100

    Update attributes check-box Contact form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -21,12 +21,19 @@ class ContactType extends AbstractType
             ->add('objet')
             ->add('message')
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'En soumettant ce formulaire, j’accepte que mes données soient utilisées dans le cadre de ma demande et de la relation qui peut en découler.
+Ces données sont collectées pour vous apporter le meilleur service et ne sont nullement vouées à être transmises à des tiers. Conformément à la loi du 6 Janvier 1978,
+vous disposez d\'un droit d\'accès, d\'opposition et de rectification de ces données. Pour l\'exercer, vous pouvez nous écrire à
+                 <a href="mailto:admini@exemple.com">l\'administrateur du site</a>.',
                 'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                'required' => true,
+                'attr' => [
+                    'class' => 'consentement-box',
                 ],
+                'row_attr' => [
+                    'class' => 'consentement',
+                ],
+                'label_html' => true, // Autoriser l'utilisation de balises HTML dans le libellé
             ])
         ;
     }
