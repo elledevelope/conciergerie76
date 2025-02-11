7035334c6d41138fbb11e65772771833b42ee031
Elhadji Seydou Sarr <elseydousarr07@outlook.com>
2025-02-11 14:54:50 +0100

    First commit

---
+++ src/Form/AccountFormType.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse',
+                'required' => true,
+            ])
+            ->add('avatarFile', FileType::class, [
+                'label' => 'Avatar',
+                'required' => false,
+                'mapped' => false, // Ne pas mapper directement à l'entité
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
