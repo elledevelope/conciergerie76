a0a59e6288dcfb4c13266f7536be84c6880b2880
chahedslimane <slimchad06000@mail.com>
2025-02-13 12:01:11 +0100

    First commit

--- migrations/Version20250213105727.php
+++ migrations/Version20250213105727.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250212135837 extends AbstractMigration
+final class Version20250213105727 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,14 @@ final class Version20250212135837 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, objet VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE service ADD phone VARCHAR(255) DEFAULT NULL, ADD website VARCHAR(255) DEFAULT NULL, ADD address VARCHAR(255) DEFAULT NULL, DROP url');
+        $this->addSql('ALTER TABLE user ADD nom VARCHAR(255) NOT NULL, ADD prenom VARCHAR(255) NOT NULL, ADD datedenaissance DATETIME NOT NULL, ADD adresse VARCHAR(255) NOT NULL, ADD avatar VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE contact');
         $this->addSql('ALTER TABLE service ADD url VARCHAR(500) DEFAULT NULL, DROP phone, DROP website, DROP address');
+        $this->addSql('ALTER TABLE user DROP nom, DROP prenom, DROP datedenaissance, DROP adresse, DROP avatar');
     }
 }
